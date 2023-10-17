4d24026ecf45002c527b9ba5e6731c633bea9de3
jsanti123 <s.posada3@utp.edu.co>
2023-10-16 20:46:07 -0500

    update

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuToggle = document.getElementById("menu-toggle");
+const navList = document.getElementById("nav-list");
+
+menuToggle.addEventListener("click", function() {
+    navList.classList.toggle("active");
+});
