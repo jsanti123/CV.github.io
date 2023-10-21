98685ac381ab883be5374774eb99d41b7fa83d29
jsanti123 <s.posada3@utp.edu.co>
2023-10-21 17:43:07 -0500

    mejora todo

--- script.js
+++ script.js
@@ -1,27 +1,16 @@
-const menuToggle = document.getElementById("menu-toggle");
-const navList = document.getElementById("main-nav");
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
 
-menuToggle.addEventListener("click", function() {
-    navList.classList.toggle("active");
+hamburger.addEventListener('click', ()=>{
+   //Animate Links
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+
+    //Hamburger Animation
+    hamburger.classList.toggle("toggle");
 });
 
 
-const elementoAnimado = document.querySelector(".blur-in-expand");
-
-const options = {
-  root: null, // Usar el viewport como el contenedor de referencia
-  rootMargin: "0px", // Margen alrededor del viewport
-  threshold: 0.5, // Cuando al menos el 50% del elemento es visible
-};
-
-const observer = new IntersectionObserver((entries, observer) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      elementoAnimado.classList.add("iniciar-animacion");
-      observer.unobserve(entry.target); // Dejar de observar una vez que se inicia la animación
-    }
-  });
-}, options);
-
-// Comenzar a observar el elemento
-observer.observe(elementoAnimado);
