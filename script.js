4684ce941b05e34ed50554980314e0a633861a74
jsanti123 <s.posada3@utp.edu.co>
2023-10-21 20:30:50 -0500

    update

--- script.js
+++ script.js
@@ -23,5 +23,22 @@ links.forEach(link => {
     });
 });
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+
 
 
